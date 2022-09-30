06d8b6ec35573637b555add1e245ab4ac9ba1572
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-09-30 17:39:26 +0300

    user auth

--- app_5/serializers.py
+++ app_5/serializers.py
@@ -21,11 +21,18 @@ class RegistrationSerializer(serializers.ModelSerializer):
             validators=[UniqueValidator(queryset=User.objects.all())]
             )
 
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password],
+    style={"input_type": "password"}
+    
+    )
+
+
+    password2 = serializers.CharField(write_only=True, required=True,
+    style={"input_type": "password"})
 
     class Meta:
         model = User
+        # fields = ('username', 'password', 'email', 'first_name', 'last_name')
         fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
         extra_kwargs = {
             'first_name': {'required': True},
--- app_5/urls.py
+++ app_5/urls.py
@@ -2,14 +2,14 @@
 from django.urls import path
 from .views import StudentList, StudentOperations, logout_view
 
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken import views
 
 from .views import RegisterView
 
 urlpatterns = [
     path('student/', StudentList.as_view()),
     path("student/<int:id>", StudentOperations.as_view(), name="detail"),
-    path('login/', obtain_auth_token, name="login"),
+    path('login/', views.obtain_auth_token, name='login'),
     path("register/", RegisterView.as_view(), name="register" ),
      path('logout/', logout_view, name="logout"),
 ]
